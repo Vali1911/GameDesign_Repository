245a626027282040c459b9ac3ba19c2c65d1eab9
Vali1911 <valentin.uhl04@gmail.com>
2025-11-27 10:51:17 +0100

    Steuerung + Begrenzung
    Basic Spielersteuerung und der Character kann nur ins nächste Panel wenn er erst das Icon aufsammelt

---
+++ GameDesign_PA/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Test
+    private float Move;
+    
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    
+    public float speed;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Bewegung
+        Move = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(Move * speed, rb.linearVelocity.y);
+
+        // Player drehen
+        if (Move > 0)
+            sr.flipX = false;    // nach rechts
+        else if (Move < 0)
+            sr.flipX = true;     // nach links
+    }
+}
